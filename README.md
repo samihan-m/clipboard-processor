df31ee5f55d9da14c2b5d73f5d3fbfdd82d82520
Samihan <smuppira@asu.edu>
2023-05-25 10:29:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# clipboard-processor
+ Python scripts to process copied text before placing it in the clipboard
---
+++ clip.py
@@ -0,0 +1,31 @@
+import pyperclip as pc
+from pynput.keyboard import Key, Listener, KeyCode
+
+do_process: bool = True
+
+def invert(key: KeyCode):
+    global do_process
+    try:
+        if key.char == '`':
+            do_process = not do_process
+            print(f"Processing: {'on' if do_process else 'off'}")
+        # TODO add key code for exiting program
+    except:
+        # print("Key is not a character")
+        pass
+
+listener = Listener(on_press = invert)
+listener.start()
+
+print("Clipboard processing on. Removing all newline characters from copied text before placing it in the clipboard.")
+print("Press '`' to toggle clipboard processing.")
+
+while True:
+    text = pc.waitForNewPaste()
+    if do_process is False:
+        continue
+    print(f"In: {repr(text)}")
+    split_text = text.replace("\r\n", "\n").split("\n")
+    processed_text = " ".join([term.strip() for term in split_text])
+    pc.copy(processed_text)
+    print(f"Out: {repr(processed_text)}")
